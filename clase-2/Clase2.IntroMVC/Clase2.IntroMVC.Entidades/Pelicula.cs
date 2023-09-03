82bba4b57a2df3b0c2268f2f661f35a1c37494ff
GabrielNaran <102691523+GabrielNara@users.noreply.github.com>
2023-09-03 15:23:00 -0300

    Se creo la pelicula
    Se creo, la entidad pelicula, su repositorio, su controlador, sus metodos y vistas, faltan igual algunas cosas como eliminar, actualizar, en series y peliculas

---
+++ clase-2/Clase2.IntroMVC/Clase2.IntroMVC.Entidades/Pelicula.cs
@@ -0,0 +1,19 @@
+﻿namespace Clase2.IntroMVC.Entidades;
+
+public class Pelicula
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public string? Genero { get; set; }
+    public string? Descripcion { get; set; }
+    public int? Año { get; set; }
+    public decimal? Presupuesto { get; set; }
+    public decimal? Recaudacion { get; set; }
+    public string? ImagenUrl { get; set; }
+    public bool? NominadaAlOscar { get; set; }
+    public bool? EstaEnNetflix { get; set; }
+    public bool? EstaEnDisney { get; set; }
+    public bool? EstaEnAmazon { get; set; }
+    public bool? EstaEnStarPlus { get; set; }
+    public bool? EstaEnHBO { get; set; }
+}
---
+++ clase-2/Clase2.IntroMVC/Clase2.IntroMVC.Web/Controllers/PeliculasController.cs
@@ -0,0 +1,38 @@
+﻿using Clase2.IntroMvc.Logica;
+using Clase2.IntroMVC.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase2.IntroMVC.Web.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private IPeliculasRepositorio _peliculasRepositorio;
+        public PeliculasController(IPeliculasRepositorio peliculasRepositorio)
+        {
+            _peliculasRepositorio = peliculasRepositorio;
+        }
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Agregar(Pelicula pelicula)
+        {
+           pelicula.EstaEnStarPlus = Request.Form["EstaEnStarPlus"] == "on";
+            pelicula.EstaEnNetflix = Request.Form["EstaEnNetflix"] == "on";
+            pelicula.EstaEnHBO = Request.Form["EstaEnHBO"] == "on";
+            pelicula.EstaEnDisney = Request.Form["EstaEnDisney"] == "on";
+            pelicula.EstaEnAmazon = Request.Form["EstaEnAmazon"] == "on"; 
+            _peliculasRepositorio.Agregar(pelicula);
+            return RedirectToAction("Listado");
+        }
+
+        public IActionResult Listado()
+        {
+            var peliculas = _peliculasRepositorio.ObtenerTodas();
+            return View(peliculas);
+        }
+
+    }
+}
