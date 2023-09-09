480188722788b4e2cf081c5711e1822b3f2f8cec
GabrielNaran <102691523+GabrielNara@users.noreply.github.com>
2023-09-09 12:17:42 -0300

    Layouts
    se generaron un layout base y uno que podes poner

---
+++ clase-3/Clase3.tarea/Clase3.tarea.web/Controllers/FormularioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clase3.tarea.web.Controllers
+{
+    public class FormularioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
