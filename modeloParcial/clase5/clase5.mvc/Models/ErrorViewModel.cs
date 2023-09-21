7ca8907271192941712211908827e205ff94905d
GabrielNaran <102691523+GabrielNara@users.noreply.github.com>
2023-09-21 18:27:34 -0300

    .
    .

---
+++ modeloParcial/clase5/clase5.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace clase5.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
