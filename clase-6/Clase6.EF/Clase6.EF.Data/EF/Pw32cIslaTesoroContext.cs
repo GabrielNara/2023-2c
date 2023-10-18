5d55b85335f8414e85074a2ea8883a98ef6b6c90
GabrielNaran <102691523+GabrielNara@users.noreply.github.com>
2023-10-18 15:03:23 -0300

    .
    .

--- clase-6/Clase6.EF/Clase6.EF.Data/EF/Pw32cIslaTesoroContext.cs
+++ clase-6/Clase6.EF/Clase6.EF.Data/EF/Pw32cIslaTesoroContext.cs
@@ -19,7 +19,7 @@ public partial class Pw32cIslaTesoroContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=Pw3-2c-IslaTesoro;Trusted_Connection=True;Encrypt=False");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-1ILI4M4\\SQLEXPRESS;Database=Pw32cIslaTesoro;Trusted_Connection=True;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
